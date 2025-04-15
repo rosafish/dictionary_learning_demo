839ba9dc355de67ccda49751b58898df14a89762
rosafish <rosazhou1997@gmail.com>
2025-04-15 17:45:25 -0500

    finalize SAEs

--- simplified_shoe.sh
+++ simplified_shoe.sh
@@ -19,13 +19,41 @@
 #     --architectures batch_top_k\
 #     --components resid
 
+# -----------------------------------------------------
 
+# python simplified_shoe.py\
+#     --save_dir attn_30\
+#     --model_name /data/rosa/work_in_progress/compositional_interpretability/outputs/shoe_simple_two_level_lr0.0005_epochs30_batch8_warmup100_pythia_cls_head\
+#     --layers 3 5\
+#     --architectures batch_top_k\
+#     --components attn\
+#     --do_train
+
+# # --do_eval
+
+# python simplified_shoe.py\
+#     --save_dir resid_30\
+#     --model_name /data/rosa/work_in_progress/compositional_interpretability/outputs/shoe_simple_two_level_lr0.0005_epochs30_batch8_warmup100_pythia_cls_head\
+#     --layers 5\
+#     --architectures batch_top_k\
+#     --components resid\
+#     --do_train
+
+# python simplified_shoe.py\
+#     --save_dir mlp_30\
+#     --model_name /data/rosa/work_in_progress/compositional_interpretability/outputs/shoe_simple_two_level_lr0.0005_epochs30_batch8_warmup100_pythia_cls_head\
+#     --layers 3\
+#     --architectures batch_top_k\
+#     --components mlp\
+#     --do_train
+
+# -----------------------------------------------------
+
+# evaluate SAEs
 python simplified_shoe.py\
-    --save_dir ./eval_on_shoe\
+    --save_dir SAEs_shoe_simple_final\
     --model_name /data/rosa/work_in_progress/compositional_interpretability/outputs/shoe_simple_two_level_lr0.0005_epochs30_batch8_warmup100_pythia_cls_head\
-    --layers 5\
+    --layers 3\
     --architectures batch_top_k\
-    --components resid\
+    --components mlp\
     --do_eval
-
-    # --do_train
